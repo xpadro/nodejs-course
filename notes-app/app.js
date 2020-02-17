286d07b92ba71cf8efec852dd6ba76ca88c7e371
Xavier Padro <xpadro.sw@gmail.com>
2020-02-17 08:26:25 +0100

    Added yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,40 @@
-const getNotes = require("./notes.js");
+const chalk = require("chalk")
+const yargs = require("yargs")
 
-console.log(getNotes());
+const getNotes = require("./notes.js")
+
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log("Adding a new note!")
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists notes',
+    handler: function () {
+        console.log('Listing the notes...')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reding the note')
+    }
+})
+
+console.log(yargs.argv)
